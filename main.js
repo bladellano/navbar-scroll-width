9fd01a80199dd4d299dfb2be5da045dc56dd3e24
Caio Silva <bladellano@gmail.com>
2019-12-18 11:09:56 -0300

    Subindo os arquivos

---
+++ main.js
@@ -0,0 +1,41 @@
+(function(doc, win) {
+
+    'use strict';
+
+    const nav = doc.querySelector('#nav');
+
+    win.onscroll = () => {
+        if (win.pageYOffset > 100) {
+            nav.style.position = 'fixed';
+            nav.style.top = 0;
+        } else {
+            nav.style.position = 'relative';
+        }
+    }
+
+    const navSlide = () => {
+        const burger = doc.querySelector('.burger');
+        const nav = doc.querySelector('.nav-links');
+        const navLinks = doc.querySelectorAll('.nav-links li');
+
+        burger.addEventListener('click', () => {
+            //Toggle Nav
+            nav.classList.toggle('nav-active');
+
+            //Animate Links
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0}s`;
+                }
+            });
+            //Burger Animation
+            burger.classList.toggle('toggle');
+        });
+
+
+    }
+    navSlide();
+
+})(document, window);
