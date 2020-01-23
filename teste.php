a579a4f69cda13f18ad82ef2edbb896933254ae8
Caio Silva <bladellano@gmail.com>
2020-01-22 21:07:17 -0300

    Adicionado plugin analytics.google.

---
+++ teste.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+	<style>
+		.boxEvento{
+			width: 300px;
+			height: 200px;
+			position: relative;
+			background: url("assets/img/plateia.jpg");
+			background-repeat: no-repeat;
+			background-position: top;
+			background-size: 100%;
+			background-color: #FFF;
+			border-radius:4px;
+			border:1px solid #CCC;			
+		}
+		.dataEvento{
+			width: 60px;
+			border:1px solid #000;
+			height: 50px;
+			line-height: 20px;
+			border-radius:4px;
+			position:absolute;
+			bottom: 18px;
+			left: 8px;
+			text-align: center;
+			background-color: #FFF;
+			border:1px solid #CCC;
+		}
+	
+		.boxEvento .titulo{
+			position: absolute;
+			bottom: 12px;	
+			left: 80px;
+			display: block;
+		}
+		.boxEvento .resumo{
+			color:#888;
+			display: block;
+		}
+	</style>
+</head>
+<body>
+	<h3>Card</h3>
+
+	<div class="boxEvento">
+		<div class="dataEvento">FEV<br/>11</div>
+		<span class="titulo">Evolução do cerébro - 2º<span>
+		<span class="resumo">Sede sinapsys Mu...</span>
+		</div>
+	</body>
+	</html>
